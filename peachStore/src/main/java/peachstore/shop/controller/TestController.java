e1d2d1abfa3b45a154117eaf8d71a837e743f12c
yudinee <apdkfldbwls@icloud.com>
2025-07-28 16:32:22 +0900

    진짜병합

--- peachStore/src/main/java/peachstore/shop/controller/TestController.java
+++ peachStore/src/main/java/peachstore/shop/controller/TestController.java
@@ -41,6 +41,9 @@ public class TestController {
 		return "redirect:/shop/main";
 		
 	}
-	
-	
+	@GetMapping("/inquiry/registForm")
+	public String inquiryRegist() {
+		
+		return "/shop/inquiry/regist";
+	}
 }
