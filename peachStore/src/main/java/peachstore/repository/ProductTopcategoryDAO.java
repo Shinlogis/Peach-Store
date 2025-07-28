99a1b43da4cf5ebc81b530edca9aa3dcecb65dbf
Kim YeJin <320418@naver.com>
2025-07-28 16:58:36 +0900

    테이블명 변경(product_category -> product_topcategory)에 따른 변수명 변경

---
+++ peachStore/src/main/java/peachstore/repository/ProductTopcategoryDAO.java
@@ -0,0 +1,43 @@
+package peachstore.repository;
+
+import java.util.List;
+
+import peachstore.domain.ProductTopcategory;
+
+
+/**
+ * 제품 상위 카테고리 DAO입니다
+ * 
+ * @author 김예진
+ * @since 2025-07-24
+ */
+public interface ProductTopcategoryDAO {
+	
+	/**
+	 * 제품 상위카테고리 전체 조회
+	 * @param productCategory
+	 * @return
+	 */
+	public List<ProductTopcategory> selectAll(String searchKey);
+	
+	/**
+	 * 제품 상위카테고리 등록
+	 * @param categoryName
+	 */
+	public void register(ProductTopcategory productTopCategory);
+	
+	/**
+	 * 제품 상위카테고리 수정
+	 * @param productTopCategory
+	 * @return update row 수
+	 */
+	public int update(ProductTopcategory productTopCategory);
+	
+	/**
+	 * pk값으로 제품 상위카테고리 조회
+	 * @param productTopCategoryId
+	 * @return
+	 */
+	public ProductTopcategory findById(int productTopCategoryId);
+
+}
