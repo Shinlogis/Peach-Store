ee3906a82195bcf0195a02ecc8f2780f21993bb1
dev-Kiwi7 <seodalmi.u@gmail.com>
2025-07-29 16:11:26 +0900

    0729 header toplist추가

--- peachStore/src/main/java/peachstore/shop/controller/MainController.java
+++ peachStore/src/main/java/peachstore/shop/controller/MainController.java
@@ -5,6 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import lombok.extern.slf4j.Slf4j;
 
+/**
+ * 메인 페이지 컨트롤러 
+ * 
+ * @author 서예닮 
+ * @since 2025-07-25
+ */
 @Slf4j
 @Controller
 public class MainController {
