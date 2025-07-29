3556597a1cb6276506dbc1809e17b2c4a35d7acb
Kim YeJin <320418@naver.com>
2025-07-29 16:25:39 +0900

    상위 카테고리에 해당하는 하위 카테고리 조회 기능 구현

--- peachStore/src/main/java/peachstore/domain/UserGrade.java
+++ peachStore/src/main/java/peachstore/domain/UserGrade.java
@@ -1,6 +1,7 @@
 package peachstore.domain;
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 //tinyInt는 boolean으로 매핑하면 된다고 함.
 	
 import java.util.List;
@@ -31,17 +32,36 @@ public class UserGrade {
 	private List<GradeCoupon> gradeCoupons;
 =======
 import java.math.BigDecimal;
+=======
+import java.util.List;
+>>>>>>> 2362fca (상위 카테고리에 해당하는 하위 카테고리 조회 기능 구현)
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+/**
+ * 회원 등급 엔티티
+ * @author 김예진 
+ * @since 2025-07-28
+ */
+@Getter
+@Setter
+@NoArgsConstructor
 public class UserGrade {
-	private int user_grade_id;
-	private String user_grade_name;
-	private BigDecimal discount_rate;
-	private int criteria_amount;
-	//tinyInt는 boolean으로 매핑하면 된다고 함.
-	private boolean isActive;
+	private int userGradeId;
+
+	private String userGradeName;
 	
+<<<<<<< HEAD
 >>>>>>> 4d436b5 (추적 제거 및 스테이지편집 후 커밋)
+=======
+	private int criteriaAmount;
+
+	private double discountRate;
+	
+	private boolean isActive;
+
+	private List<GradeCoupon> gradeCoupons;
+>>>>>>> 2362fca (상위 카테고리에 해당하는 하위 카테고리 조회 기능 구현)
 }
