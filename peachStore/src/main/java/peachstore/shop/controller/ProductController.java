32397b5bc7ae59d13f498591829040c786b5e0b6
dev-Kiwi7 <seodalmi.u@gmail.com>
2025-07-29 20:16:43 +0900

    0729 예닮 header img 수정

--- peachStore/src/main/java/peachstore/shop/controller/ProductController.java
+++ peachStore/src/main/java/peachstore/shop/controller/ProductController.java
@@ -1,14 +1,41 @@
 package peachstore.shop.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import peachstore.domain.ProductSubcategory;
+import peachstore.domain.ProductTopcategory;
+import peachstore.service.ProductSubcategoryService;
+import peachstore.service.ProductTopcategoryService;
+
+/**
+ * 제품컨트롤러 
+ * 
+ * @author 서예닮 
+ * @since 2025-07-29 
+ */
 @Controller
 public class ProductController {
+	@Autowired
+	ProductSubcategoryService productSubcategoryService;
+	@Autowired
+	ProductTopcategoryService productTopcategoryService;
 	
-	@GetMapping("/product/pPhone")
-	public String phone() {
-		return "shop/product/pPhone";
+	@GetMapping("/product")
+	public ModelAndView phone(int topid) {
+		List<ProductSubcategory> subList=productSubcategoryService.findAllByTopcategoryId(topid);
+		ProductTopcategory productTopcategory=productTopcategoryService.findById(topid);
+		String topName=productTopcategory.getProductTopcategoryName();
+		
+		ModelAndView mav = new ModelAndView("shop/product/"+productTopcategory.getProductTopcategoryName());
+		mav.addObject("subList", subList);
+		mav.addObject("topName", topName);
+		
+		return mav;
 	}
 	
 	@GetMapping("/product/pPad")
