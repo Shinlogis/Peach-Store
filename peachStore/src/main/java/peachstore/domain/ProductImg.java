0008506e73d8bf6f014267a82876140a92f543bf
shypanda0119 <kmkm7936@naver.com>
2025-07-30 11:29:24 +0900

    패키지 정리

--- peachStore/src/main/java/peachstore/domain/ProductImg.java
+++ peachStore/src/main/java/peachstore/domain/ProductImg.java
@@ -6,7 +6,7 @@ package peachstore.domain;
  * @since 2025-07-28
  */
 public class ProductImg {
-	private int product_img_id;
+	private int product_img_id; //
 	private String filename;
 	
 	private Product product;
