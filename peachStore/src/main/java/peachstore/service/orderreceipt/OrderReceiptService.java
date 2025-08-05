1ae5974f458019bc31788cc78b2c467be950e2e4
Kim YeJin <320418@naver.com>
2025-08-05 14:01:10 +0900

    결제 정보 저장 메서드 정리

--- peachStore/src/main/java/peachstore/service/orderreceipt/OrderReceiptService.java
+++ peachStore/src/main/java/peachstore/service/orderreceipt/OrderReceiptService.java
@@ -26,12 +26,11 @@ public interface OrderReceiptService {
 	
 	/**
 	 * 주문내역 생성
-	 * 
-	 * @param localDateTime 주문 시각(=결제완료 시각)
-	 * @param orderStatus 상품 준비 상태
-	 * @param user 주문한 사용자
-	 * @param paymentId 결제번호
-	 * @return
+	 * @param localDateTime
+	 * @param orderStatus
+	 * @param user
+	 * @param tosspayment
+	 * @return OrderReceipt 등록한 주문내역 반환
 	 */
-	public void insert(LocalDateTime localDateTime, String orderStatus, User user, Tosspayment tosspayment);
+	public OrderReceipt insert(LocalDateTime localDateTime, String orderStatus, User user, Tosspayment tosspayment);
 }
